008f3c143adc369b81bd8b21afb1f56179a22399
j-morris-112358 <j_morris_112358@yahoo.com>
2024-10-11 11:24:05 -0400

    de-flaking routes.py

--- service/routes.py
+++ service/routes.py
@@ -57,9 +57,10 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
-######################################################################    
+######################################################################
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -85,11 +86,12 @@ def get_accounts(account_id):
     This endpoint will read an Account based the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -107,6 +109,7 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # DELETE AN ACCOUNT
